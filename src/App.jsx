0a080ba19ad15fcc9e88596879bf7bd8a061d8f0
shubham tyagi <shubham.tyagi_bca24@gla.ac.in>
2026-08-17 23:31:07 +0800

    fix usescrollReveal import

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Skills from "./skills";
 import Projects from "./project";
 import Education from "./education";
 import Contact from "./contact";
-import useScrollReveal from "./useScrollreveal";
+import useScrollReveal from "./usescrollReveal";
 
 function App() {
   useScrollReveal();
